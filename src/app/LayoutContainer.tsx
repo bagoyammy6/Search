6fbfc98531697d90b84c8d897d7e642f1849a049
Rocky Chou <r0918375004@gmail.com>
2024-04-06 01:35:31 +0800

    完成user search

---
+++ src/app/LayoutContainer.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React from 'react';
+import { ThemeProvider } from 'next-themes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function LayoutContainer({ children }: Props) {
+  return (
+    <ThemeProvider attribute='class'>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ThemeProvider>
+  );
+}
---
+++ src/app/components/ToggleBtn.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React from 'react';
+import { IoSunnyOutline } from 'react-icons/io5';
+import { useTheme } from 'next-themes';
+import { LuMoonStar } from 'react-icons/lu';
+
+export default function ToggleBtn() {
+  const { resolvedTheme, setTheme } = useTheme();
+
+  function toggleDark() {
+    if (resolvedTheme === 'light') setTheme('dark');
+    if (resolvedTheme === 'dark') setTheme('light');
+  }
+
+  return (
+    <div className='flex items-center gap-2'>
+      <p className='text-sm'>{resolvedTheme === 'light' ? 'DARK' : 'LIGHT'}</p>
+      <button onClick={toggleDark} className='text-2xl'>
+        {resolvedTheme === 'light' ? <LuMoonStar /> : <IoSunnyOutline />}
+      </button>
+    </div>
+  );
+}
